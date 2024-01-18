22d6442b7cce5d1eeff063f66d2db80d7e766385
Darchiashvili9 <gdarchiashvili@hotmail.com>
2024-01-18 22:02:57 +0400

    added entities. migrations. and updated db using configurations

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,4 @@
-using Core.Interfaces;
+﻿using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,10 +6,9 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
 
             builder.Services.AddControllers();
             builder.Services.AddDbContext<StoreContext>(c => c.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -19,23 +18,35 @@ namespace API
 
             var app = builder.Build();
 
-
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+
+                    //ეს გააკეთებს ბაზის აფდეით არსებული მიგრაციებით, ისე რო ჩვენით დატაბაზის აფდეითი არ გვჭირდება. თავისით მიხედავს დასტარტვაზე
+                    await context.Database.MigrateAsync();
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "migraciis dros moxda erori");
+                }
+            }
 
             app.Run();
         }
