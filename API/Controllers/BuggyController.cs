739a2698e1359ba924a03e3538ac8ce3b254e017
Darchiashvili9 <gdarchiashvili@hotmail.com>
2024-02-01 20:01:15 +0400

    error handling added

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,48 @@
+﻿using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        StoreContext _storeContext;
+        public BuggyController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _storeContext.Products.Find(42);
+
+            if (thing == null)
+                return NotFound(new ApiResponse(404));
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var thing = _storeContext.Products.Find(42);
+
+            var thingToReturn = thing.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequect()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequect(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public ApiResponse(int statusCode, string errorMessage = null)
+        {
+            StatusCode = statusCode;
+            ErrorMessage = errorMessage ?? GetDefaultMessage(statusCode);
+        }
+
+        private string GetDefaultMessage(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "a bad request, you have made",
+                401 => "you are not authorized",
+                404 => "resource not found",
+                500 => "server error",
+                _ => null
+            };
+        }
+    }
+}
