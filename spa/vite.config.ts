c28def9ffe69c0a61f6cbce5340c142201f2fe3a
Darchiashvili9 <gdarchiashvili@hotmail.com>
2024-02-05 19:10:02 +0400

    spa

---
+++ spa/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), mkcert()],
+    server: {
+        port: 4000,
+        strictPort: false,
+    },
+    preview: {
+        port: 4173,
+        strictPort: false,
+    }
+
+})
