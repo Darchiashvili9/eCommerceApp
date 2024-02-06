ba071e0173182a21d736fb7e998dbf13a88ef1aa
Darchiashvili9 <gdarchiashvili@hotmail.com>
2024-02-06 16:52:54 +0400

    fetch data

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -1,10 +1,56 @@
 import './App.css'
 import navbar from "./Components/nav-bar/navbar"
+import { useEffect, useState } from 'react';
 
 
-
-
+interface Forecast {
+    date: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
+}
 function App() {
+    async function populateWeatherData() {
+        const response = await fetch('https://localhost:5001/weatherforecast');
+        const data = await response.json();
+        setForecasts(data);
+    }
+
+    const [forecasts, setForecasts] = useState<Forecast[]>();
+
+    useEffect(() => {
+        populateWeatherData();
+    }, []);
+
+
+    const contents = forecasts === undefined
+        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+        : <table className="table table-striped" aria-labelledby="tabelLabel">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Temp. (C)</th>
+                    <th>Temp. (F)</th>
+                    <th>Summary</th>
+                </tr>
+            </thead>
+            <tbody>
+                {forecasts.map(forecast =>
+                    <tr key={forecast.date}>
+                        <td>{forecast.date}</td>
+                        <td>{forecast.temperatureC}</td>
+                        <td>{forecast.temperatureF}</td>
+                        <td>{forecast.summary}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>;
+
+
+
+
+
+
     return (
 
         <div className="App">
@@ -15,6 +61,10 @@ function App() {
                 <h1>
                     spa
                 </h1>
+
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+
             </div>
 
         </div>
