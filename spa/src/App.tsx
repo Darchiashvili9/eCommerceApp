ba7bda47be2557ac8fe18a089a30b849c1142a3c
Darchiashvili9 <gdarchiashvili@hotmail.com>
2024-02-07 14:58:20 +0400

    fetchfromApi

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -1,29 +1,29 @@
 import './App.css'
+import { IPagination } from './Components/models/pagination';
+import { IProduct } from './Components/models/products';
 import navbar from "./Components/nav-bar/navbar"
 import { useEffect, useState } from 'react';
 
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
+
 function App() {
     async function populateWeatherData() {
-        const response = await fetch('https://localhost:5001/weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
+        const resp = await fetch('https://localhost:5001/api/products?pageSize=50');
+        const response: IPagination = await resp.json();
+
+        console.log(response.data)
+
+        setForecasts(response.data);
     }
 
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+    const [products, setForecasts] = useState<IProduct[]>();
 
     useEffect(() => {
         populateWeatherData();
     }, []);
 
 
-    const contents = forecasts === undefined
+    const contents = products === undefined
         ? <p>Loading... Please refresh once the ASP.NET backend has started </p>
         : <table className="table table-striped" aria-labelledby="tabelLabel">
             <thead>
@@ -32,15 +32,19 @@ function App() {
                     <th>Temp. (C)</th>
                     <th>Temp. (F)</th>
                     <th>Summary</th>
+                    <th>Summary</th>
                 </tr>
             </thead>
             <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
+                {products.map(products =>
+                    <tr key={products.description}>
+                        <td>{products.name}</td>
+                        <td>{products.id}</td>
+                        <td>{products.pictureUrl}</td>
+                        <td>{products.productBrand}</td>
+                        <td>{products.productType}</td>
+                        <td>{products.price}</td>
+
                     </tr>
                 )}
             </tbody>
