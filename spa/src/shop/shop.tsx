4f8f37b4d5e961046970e572b16d35149f50b52a
Darchiashvili9 <gdarchiashvili@hotmail.com>
2024-02-10 20:11:10 +0400

    productItemPage

--- spa/src/shop/shop.tsx
+++ spa/src/shop/shop.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { IProduct } from '../shared/models/products';
 import ShopService from "./shop.service";
 import "./shop.css";
+import ProductItem from "./product-item";
 
 function Shop() {
     var [products, setProducts] = useState<IProduct[]>();
@@ -17,6 +18,7 @@ function Shop() {
             }
         })();
     }, []);
+
 
     return (
         //<div>
@@ -92,173 +94,27 @@ function Shop() {
                     </div>
 
 
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                        <div className="col" bis_skin_checked="1">
-                            <div className="card shadow-sm" bis_skin_checked="1">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                <div className="card-body" bis_skin_checked="1">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <div className="d-flex justify-content-between align-items-center" bis_skin_checked="1">
-                                        <div className="btn-group" bis_skin_checked="1">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                        </div>
-                                        <small className="text-body-secondary">9 mins</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col" bis_skin_checked="1">
-                            <div className="card shadow-sm" bis_skin_checked="1">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                <div className="card-body" bis_skin_checked="1">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <div className="d-flex justify-content-between align-items-center" bis_skin_checked="1">
-                                        <div className="btn-group" bis_skin_checked="1">
-                                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                        </div>
-                                        <small className="text-body-secondary">9 mins</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col" bis_skin_checked="1">
-                            <div className="card shadow-sm" bis_skin_checked="1">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                <div className="card-body" bis_skin_checked="1">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <div className="d-flex justify-content-between align-items-center" bis_skin_checked="1">
-                                        <div className="btn-group" bis_skin_checked="1">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                        </div>
-                                        <small className="text-body-secondary">9 mins</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div className="col" bis_skin_checked="1">
-                            <div className="card shadow-sm" bis_skin_checked="1">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                <div className="card-body" bis_skin_checked="1">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <div className="d-flex justify-content-between align-items-center" bis_skin_checked="1">
-                                        <div className="btn-group" bis_skin_checked="1">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                        </div>
-                                        <small className="text-body-secondary">9 mins</small>
+                    <div className="row" style={{ borderColor: "black", borderWidth: "5px", border: "solid" }}>
+                        <div className="d-flex flex-row flex-wrap col-4">
+                            {
+                                products?.map((ind) =>
+                                    <div key={ind.id}>
+                                        <ProductItem />
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col" bis_skin_checked="1">
-                            <div className="card shadow-sm" bis_skin_checked="1">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                <div className="card-body" bis_skin_checked="1">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <div className="d-flex justify-content-between align-items-center" bis_skin_checked="1">
-                                        <div className="btn-group" bis_skin_checked="1">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                        </div>
-                                        <small className="text-body-secondary">9 mins</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col" bis_skin_checked="1">
-                            <div className="card shadow-sm" bis_skin_checked="1">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                <div className="card-body" bis_skin_checked="1">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <div className="d-flex justify-content-between align-items-center" bis_skin_checked="1">
-                                        <div className="btn-group" bis_skin_checked="1">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                        </div>
-                                        <small className="text-body-secondary">9 mins</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col" bis_skin_checked="1">
-                            <div className="card shadow-sm" bis_skin_checked="1">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                <div className="card-body" bis_skin_checked="1">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <div className="d-flex justify-content-between align-items-center" bis_skin_checked="1">
-                                        <div className="btn-group" bis_skin_checked="1">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                        </div>
-                                        <small className="text-body-secondary">9 mins</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col" bis_skin_checked="1">
-                            <div className="card shadow-sm" bis_skin_checked="1">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                <div className="card-body" bis_skin_checked="1">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <div className="d-flex justify-content-between align-items-center" bis_skin_checked="1">
-                                        <div className="btn-group" bis_skin_checked="1">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                        </div>
-                                        <small className="text-body-secondary">9 mins</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col" bis_skin_checked="1">
-                            <div className="card shadow-sm" bis_skin_checked="1">
-                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                <div className="card-body" bis_skin_checked="1">
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <div className="d-flex justify-content-between align-items-center" bis_skin_checked="1">
-                                        <div className="btn-group" bis_skin_checked="1">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                        </div>
-                                        <small className="text-body-secondary">9 mins</small>
-                                    </div>
-                                </div>
-                            </div>
+                                )
+                            }
                         </div>
                     </div>
+
+
+
+
 
 
                 </section>
             </div>
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     );
 }
 
